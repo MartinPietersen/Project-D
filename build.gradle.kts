65b1147c0a714678f7406d50ed6e38513fcd7e4c
Boy <52785948+Boy-Stroo@users.noreply.github.com>
2024-05-10 17:05:49 +0200

    Revert "Switch to Compose Multiplatform"
    This reverts commit b9d39e0260d41f9ecc33f5952c01da377aa7e152.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    //trick: for the same plugin versions in all sub-modules
-    alias(libs.plugins.androidApplication).apply(false)
-    alias(libs.plugins.androidLibrary).apply(false)
-    alias(libs.plugins.kotlinAndroid).apply(false)
-    alias(libs.plugins.kotlinMultiplatform).apply(false)
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,24 @@
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 pluginManagement {
     repositories {
-        google()
-        gradlePluginPortal()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
+        gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
 }
 
-rootProject.name = "My_Application"
-include(":BAMA")
-include(":shared")
+rootProject.name = "BAMA"
+include(":app")
+ 
