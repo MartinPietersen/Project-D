dcfc310e9cf04dccca2c43ceeebc63ec6c18d7e6
Boy <52785948+Boy-Stroo@users.noreply.github.com>
2024-05-13 21:08:21 +0200

    One more try
    One more try to set up

--- app/src/main/java/com/example/bama/ui/theme/Color.kt
+++ app/src/main/java/com/example/bama/ui/theme/Color.kt
@@ -10,10 +10,10 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val GrayDark = Color(0xFF333333)
+val GrayDark = Color(0xFF444444)
 val GrayLight = Color(0xFF999999)
 val Gray = Color(0xFF666666)
 
 val Green = Color(0xFF107200)
-val GreenDark = Color(0xFF0D5E00)
-val GreenLight = Color(0xFFC7FFB2)
+val GreenLight = Color(0xFFD6FFC5)
+val GreenDark = Color(0xFF004D00)
