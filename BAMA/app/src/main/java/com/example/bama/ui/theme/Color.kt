330d6e71d74a73262f87d027eebcf4ef8823949e
Boy <52785948+Boy-Stroo@users.noreply.github.com>
2024-05-13 20:26:44 +0200

    back to square one
    Everything reverted to jetpack compose

--- BAMA/app/src/main/java/com/example/bama/ui/theme/Color.kt
+++ BAMA/app/src/main/java/com/example/bama/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val GrayDark = Color(0xFF333333)
+val GrayLight = Color(0xFF999999)
+val Gray = Color(0xFF666666)
+
+val Green = Color(0xFF107200)
+val GreenDark = Color(0xFF0D5E00)
+val GreenLight = Color(0xFFC7FFB2)
